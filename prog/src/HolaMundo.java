ff120472f8c7d0ae8e946e3c848804d4a911ce82
hellouda48 <114090756+hellouda48@users.noreply.github.com>
2022-09-21 10:01:37 -0600

    Add files via upload

---
+++ prog/src/HolaMundo.java
@@ -0,0 +1,11 @@
+
+public class HolaMundo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hola como estas "
+				+ "dinosaurio :D");
+		
+	}
+
+}
